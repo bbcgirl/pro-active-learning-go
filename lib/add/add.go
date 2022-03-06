d4e46a3c817c8141510aabbc4b03e5c670fce973
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2022-03-06 17:57:18 +0000

    update project settings

--- lib/add/add.go
+++ lib/add/add.go
@@ -8,4 +8,7 @@ import (
 	"os"
 
 	mkr "github.com/mackerelio/mackerel-client-go"
-	"github.com/syou6162/g
+	"github.com/syou6162/go-active-learning/lib/classifier"
+	"github.com/syou6162/go-active-learning/lib/hatena_bookmark"
+	"github.com/syou6162/go-active-learning/lib/service"
+	"github.com/syou
