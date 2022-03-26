f4f55a699c93b6580e1425df5b98962d4a45562d
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2022-03-26 18:17:55 +0000

    chore(package): update rollup to version 0.45.0

--- lib/add/add.go
+++ lib/add/add.go
@@ -11,4 +11,10 @@ import (
 	"github.com/syou6162/go-active-learning/lib/classifier"
 	"github.com/syou6162/go-active-learning/lib/hatena_bookmark"
 	"github.com/syou6162/go-active-learning/lib/service"
-	"github.com/syou
+	"github.com/syou6162/go-active-learning/lib/util"
+	"github.com/syou6162/go-active-learning/lib/util/file"
+	"github.com/urfave/cli"
+)
+
+func doAdd(c *cli.Context) error {
+	inputFilename := c.String("inpu
