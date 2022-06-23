8c674e2130b14bf05c846ba4c4cb991f19cdcf72
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2022-06-23 22:33:50 +0000

    um: Skip futex_atomic_cmpxchg_inatomic() test

--- lib/annotation/annotation.go
+++ lib/annotation/annotation.go
@@ -1,4 +1,12 @@
 package annotation
 
 import (
-	"github.com/syou6162/go-active
+	"github.com/syou6162/go-active-learning/lib/classifier"
+	"github.com/syou6162/go-active-learning/lib/model"
+	"github.com/urfave/cli"
+)
+
+type ActionType int
+
+const (
+	LABEL_AS_POSITIVE ActionTyp
