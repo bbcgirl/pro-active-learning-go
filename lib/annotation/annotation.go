c41306ea8c7e5fd51df5cc92b7f06204eb6a0359
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2022-07-20 08:44:57 +0000

    Added localization

--- lib/annotation/annotation.go
+++ lib/annotation/annotation.go
@@ -68,4 +68,11 @@ Annotate URLs using active learning using cli.
 `,
 			Action: doAnnotate,
 			Flags: []cli.Flag{
-				cli.BoolFlag{Name: "open-url", Usage: "Open url in backgroun
+				cli.BoolFlag{Name: "open-url", Usage: "Open url in background"},
+				cli.BoolFlag{Name: "filter-status-code-ok", Usage: "Use only examples with status code = 200"},
+				cli.BoolFlag{Name: "show-active-features"},
+			},
+		},
+		{
+			Name:  "slack",
+			U
