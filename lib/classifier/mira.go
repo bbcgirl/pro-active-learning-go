7d0239a8aa82011070c378632ebce76ac5be0b9a
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2022-12-09 14:49:25 +0000

    add debug option

--- lib/classifier/mira.go
+++ lib/classifier/mira.go
@@ -5,4 +5,11 @@ import (
 	"math"
 	"math/rand"
 	"os"
-	
+	"runtime"
+	"sort"
+	"sync"
+
+	"github.com/pkg/errors"
+	"github.com/syou6162/go-active-learning/lib/evaluation"
+	"github.com/syou6162/go-active-learning/lib/feature"
+	"github.com/syou6162/go-active-le
