0879d4ebbfe2c8487d42eb578f328860dd43681e
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-01-02 11:40:42 +0000

    fix license headers

--- lib/classifier/mira.go
+++ lib/classifier/mira.go
@@ -12,4 +12,16 @@ import (
 	"github.com/pkg/errors"
 	"github.com/syou6162/go-active-learning/lib/evaluation"
 	"github.com/syou6162/go-active-learning/lib/feature"
-	"github.com/syou6162/go-active-le
+	"github.com/syou6162/go-active-learning/lib/model"
+	"github.com/syou6162/go-active-learning/lib/util"
+)
+
+type ModelType int
+
+const (
+	EXAMPLE ModelType = 0
+	TWITTER ModelType = 1
+)
+
+type MIRAClassifier struct {
+	ModelType Mode
