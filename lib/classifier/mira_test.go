84cdfd8b3c8b2ba5f0b40b1f7df13dba9fb0d9b5
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-02-28 22:26:04 +0000

    Version bump to 0.2.2

--- lib/classifier/mira_test.go
+++ lib/classifier/mira_test.go
@@ -3,4 +3,9 @@ package classifier
 import (
 	"testing"
 
-	"github.com/syou6162/go-acti
+	"github.com/syou6162/go-active-learning/lib/example"
+	"github.com/syou6162/go-active-learning/lib/model"
+)
+
+func TestPredictScore(t *testing.T) {
+	e1 := example.NewExample("https://b.hatena.
