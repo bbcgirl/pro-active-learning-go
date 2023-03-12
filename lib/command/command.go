28cd120a529335ae2fc954cfaf3c4544204d785d
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-03-12 15:32:21 +0000

    Fixed typo in Readme

--- lib/command/command.go
+++ lib/command/command.go
@@ -1,4 +1,7 @@
 package command
 
 import (
-	"github.com/syou616
+	"github.com/syou6162/go-active-learning/lib/add"
+	"github.com/syou6162/go-active-learning/lib/annotation"
+	"github.com/syou6162/go-active-learning/lib/diagnosis"
+	"github.com/syou6162/go-active-learni
