6214f9b94c1511d5656e54b8486ce355c28c7c66
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-03-13 10:41:45 +0000

    add ignore files

--- lib/command/command.go
+++ lib/command/command.go
@@ -4,4 +4,11 @@ import (
 	"github.com/syou6162/go-active-learning/lib/add"
 	"github.com/syou6162/go-active-learning/lib/annotation"
 	"github.com/syou6162/go-active-learning/lib/diagnosis"
-	"github.com/syou6162/go-active-learni
+	"github.com/syou6162/go-active-learning/lib/related_example"
+	"github.com/syou6162/go-active-learning/lib/top_accessed_example"
+	"github.com/urfave/cli"
+)
+
+var Commands = []cli.Command{
+	add.CommandAdd,
+	related_example.Comma
