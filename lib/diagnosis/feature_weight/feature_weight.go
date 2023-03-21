b893bae5e7e7dc0a3070f36cc2bcceeab85b15de
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-03-21 20:34:10 +0000

    Setting version to 0.2.1

--- lib/diagnosis/feature_weight/feature_weight.go
+++ lib/diagnosis/feature_weight/feature_weight.go
@@ -5,4 +5,12 @@ import (
 	"sort"
 
 	"github.com/syou6162/go-active-learning/lib/classifier"
-	"github.com/syou6162/go-active-learni
+	"github.com/syou6162/go-active-learning/lib/service"
+	"github.com/syou6162/go-active-learning/lib/util"
+	"github.com/syou6162/go-active-learning/lib/util/converter"
+	"github.com/urfave/cli"
+)
+
+type Feature struct {
+	Key    string
+	
