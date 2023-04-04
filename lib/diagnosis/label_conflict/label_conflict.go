b3753df02bcc750b54473fa0a86b7f869e1fa2ec
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-04-04 17:50:32 +0000

    Upgrade to bundles-pom 13

--- lib/diagnosis/label_conflict/label_conflict.go
+++ lib/diagnosis/label_conflict/label_conflict.go
@@ -12,4 +12,12 @@ import (
 	"github.com/syou6162/go-active-learning/lib/model"
 	"github.com/syou6162/go-active-learning/lib/service"
 	"github.com/syou6162/go-active-learning/lib/util"
-	"github.com/syou6162/go-active-learning/lib/uti
+	"github.com/syou6162/go-active-learning/lib/util/converter"
+	"github.com/urfave/cli"
+)
+
+func DoLabelConflict(c *cli.Context) error {
+	filterStatusCodeOk := c.Bool("filter-status-code-ok")
+
+	app, err := service.NewDefaultApp()
+	if 
