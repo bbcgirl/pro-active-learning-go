9bc17210e8fbaa4408b23538e176b3d25be8b3d0
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-04-04 12:49:21 +0000

    update

--- lib/diagnosis/label_conflict/label_conflict.go
+++ lib/diagnosis/label_conflict/label_conflict.go
@@ -9,4 +9,7 @@ import (
 	"encoding/csv"
 
 	"github.com/syou6162/go-active-learning/lib/classifier"
-	"githu
+	"github.com/syou6162/go-active-learning/lib/model"
+	"github.com/syou6162/go-active-learning/lib/service"
+	"github.com/syou6162/go-active-learning/lib/util"
+	"github.com/syou6162/go-active-learning/lib/uti
