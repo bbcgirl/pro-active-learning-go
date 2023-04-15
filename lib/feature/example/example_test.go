b3c461eda962e319991d6c1a52a3bc39ea67df72
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-04-15 18:45:33 +0000

    added filters

--- lib/feature/example/example_test.go
+++ lib/feature/example/example_test.go
@@ -1,4 +1,15 @@
 package example_feature
 
 import (
-	"f
+	"fmt"
+	"testing"
+)
+
+func TestIsJapanese(t *testing.T) {
+	text := "ほげ"
+	if !isJapanese(text) {
+		t.Error(fmt.Printf("%s should be Japanese", text))
+	}
+	text = "文献紹介 / Youtube"
+	if !isJapanese(text) {
+		t.Err
