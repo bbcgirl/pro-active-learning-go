fa268acf5c41790daf70fc0aadc39c036c5b34b7
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-04-25 08:06:46 +0000

    memory

--- lib/feature/feature.go
+++ lib/feature/feature.go
@@ -13,3 +13,12 @@ func (fv *FeatureVector) MarshalBinary() ([]byte, error) {
 		return nil, err
 	}
 	return []byte(json), nil
+}
+
+func (fv *FeatureVector) UnmarshalBinary(data []byte) error {
+	err := json.Unmarshal(data, fv)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ lib/feature/tweet/tweet.go
@@ -0,0 +1,187 @@
+
+package tweet_feature
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/syou6162/go-active-learning/lib/feature"
+	"github.com/syou6162/go-active-learning/lib/model"
+	"gopkg.in/vmarkovtsev/go-lcss.v1"
+)
+
+type ExampleAndTweet struct {
+	example       *model.Example
+	tweet         *model.Tweet
+	lcsLen        int
+	atMarksCnt    int
+	hashTagsCnt   int
+	cleanedText   string
+	cleanedLcsLen int
+}
+
+func (et *ExampleAndTweet) GetLabel() model.LabelType {
+	return et.tweet.Label
+}
+
+func GetExampleAndTweet(e *model.Example, t *model.Tweet) ExampleAndTweet {
+	result := ExampleAndTweet{example: e, tweet: t}
+	result.lcsLen = GetLCSLen(e.Title, t.FullText)
+
+	atRegexp := regexp.MustCompile(`@[^ ]+`)
+	result.atMarksCnt = len(atRegexp.FindAllStringSubmatch(t.FullText, -1))
+	str := atRegexp.ReplaceAllString(t.FullText, "")
+	hashRegexp := regexp.MustCompile(`#[^ ]+`)
+	result.hashTagsCnt = len(hashRegexp.FindAllStringSubmatch(t.FullText, -1))
+	result.cleanedText = hashRegexp.ReplaceAllString(str, "")
+	result.cleanedLcsLen = GetLCSLen(e.Title, result.cleanedText)
+	return result
+}
+
+func GetLCSLen(str1 string, str2 string) int {
+	return len(string(lcss.LongestCommonSubstring([]byte(str1), []byte(str2))))
+}
+
+func LCSLenFeature(et ExampleAndTweet) string {
+	prefix := "LCSLenFeature"
+	len := et.lcsLen
+	switch {
+	case len == 0:
+		return fmt.Sprintf("%s:0", prefix)
+	case len < 5:
+		return fmt.Sprintf("%s:5", prefix)
+	case len < 10:
+		return fmt.Sprintf("%s:10", prefix)
+	case len < 25:
+		return fmt.Sprintf("%s:25", prefix)
+	case len < 50:
+		return fmt.Sprintf("%s:50", prefix)
+	case len < 100:
+		return fmt.Sprintf("%s:100", prefix)
+	default:
+		return fmt.Sprintf("%s:INF", prefix)
+	}
+}
+
+func CleanedLCSLenFeature(et ExampleAndTweet) string {
+	prefix := "CleanedLCSLenFeature"
+	len := et.cleanedLcsLen
+	switch {
+	case len == 0:
+		return fmt.Sprintf("%s:0", prefix)
+	case len < 5:
+		return fmt.Sprintf("%s:5", prefix)
+	case len < 10:
+		return fmt.Sprintf("%s:10", prefix)
+	case len < 25:
+		return fmt.Sprintf("%s:25", prefix)
+	case len < 50:
+		return fmt.Sprintf("%s:50", prefix)
+	case len < 100:
+		return fmt.Sprintf("%s:100", prefix)
+	default:
+		return fmt.Sprintf("%s:INF", prefix)
+	}
+}
+
+func LCSRatioFeature(et ExampleAndTweet) string {
+	prefix := "LCSRatioFeature"
+	ratio := float64(et.lcsLen) / float64(len(et.tweet.FullText))
+	switch {
+	case ratio == 0.0:
+		return fmt.Sprintf("%s:0.0", prefix)
+	case ratio < 0.1:
+		return fmt.Sprintf("%s:0.1", prefix)
+	case ratio < 0.25:
+		return fmt.Sprintf("%s:0.25", prefix)
+	case ratio < 0.5:
+		return fmt.Sprintf("%s:0.5", prefix)
+	case ratio < 0.75:
+		return fmt.Sprintf("%s:0.75", prefix)
+	case ratio < 0.9:
+		return fmt.Sprintf("%s:0.0", prefix)
+	default:
+		return fmt.Sprintf("%s:1.0", prefix)
+	}
+}
+
+func CleanedLCSRatioFeature(et ExampleAndTweet) string {
+	prefix := "CleanedLCSRatioFeature"
+	ratio := float64(et.cleanedLcsLen) / float64(len(et.tweet.FullText))
+	switch {
+	case ratio == 0.0:
+		return fmt.Sprintf("%s:0.0", prefix)
+	case ratio < 0.1:
+		return fmt.Sprintf("%s:0.1", prefix)
+	case ratio < 0.25:
+		return fmt.Sprintf("%s:0.25", prefix)
+	case ratio < 0.5:
+		return fmt.Sprintf("%s:0.5", prefix)
+	case ratio < 0.75:
+		return fmt.Sprintf("%s:0.75", prefix)
+	case ratio < 0.9:
+		return fmt.Sprintf("%s:0.0", prefix)
+	default:
+		return fmt.Sprintf("%s:1.0", prefix)
+	}
+}
+
+func FavoriteCountFeature(et ExampleAndTweet) string {
+	prefix := "FavoriteCountFeature"
+	cnt := et.tweet.FavoriteCount
+	switch {
+	case cnt == 0:
+		return fmt.Sprintf("%s:0", prefix)
+	case cnt == 1:
+		return fmt.Sprintf("%s:1", prefix)
+	case cnt <= 3:
+		return fmt.Sprintf("%s:3", prefix)
+	case cnt <= 5:
+		return fmt.Sprintf("%s:5", prefix)
+	case cnt <= 10:
+		return fmt.Sprintf("%s:10", prefix)
+	case cnt <= 25:
+		return fmt.Sprintf("%s:25", prefix)
+	case cnt <= 50:
+		return fmt.Sprintf("%s:50", prefix)
+	case cnt <= 100:
+		return fmt.Sprintf("%s:100", prefix)
+	default:
+		return fmt.Sprintf("%s:INF", prefix)
+	}
+}
+
+func RetweetCountFeature(et ExampleAndTweet) string {
+	prefix := "RetweetCountFeature"
+	cnt := et.tweet.RetweetCount
+	switch {
+	case cnt == 0:
+		return fmt.Sprintf("%s:0", prefix)
+	case cnt == 1:
+		return fmt.Sprintf("%s:1", prefix)
+	case cnt <= 3:
+		return fmt.Sprintf("%s:3", prefix)
+	case cnt <= 5:
+		return fmt.Sprintf("%s:5", prefix)
+	case cnt <= 10:
+		return fmt.Sprintf("%s:10", prefix)
+	case cnt <= 25:
+		return fmt.Sprintf("%s:25", prefix)
+	case cnt <= 50:
+		return fmt.Sprintf("%s:50", prefix)
+	case cnt <= 100:
+		return fmt.Sprintf("%s:100", prefix)
+	default:
+		return fmt.Sprintf("%s:INF", prefix)
+	}
+}
+
+func AtMarksCountFeature(et ExampleAndTweet) string {
+	prefix := "AtMarksCountFeature"
+	cnt := et.atMarksCnt
+	switch {
+	case cnt == 0:
+		return fmt.Sprintf("%s:0", prefix)
+	case cnt == 1:
+		return fmt.Sprintf("%s:1", prefix)
+	case cnt <= 3:
