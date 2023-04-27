2bd8653e30b0f813d3ebe273026fb0aa2b6eba11
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-04-27 09:00:24 +0000

    Update class

--- lib/feature/tweet/tweet_test.go
+++ lib/feature/tweet/tweet_test.go
@@ -11,4 +11,10 @@ import (
 func TestExtractHostFeature(t *testing.T) {
 	e := model.Example{}
 	e.Title = "Hello world"
-	tw
+	tweet := model.Tweet{}
+	tweet.ScreenName = "syou6162"
+	tweet.FullText = "Hello world @syou6162 @syou6163 #hashtag1 #hashtag2"
+	tweet.FavoriteCount = 7
+	tweet.RetweetCount = 7
+
+	et := 
