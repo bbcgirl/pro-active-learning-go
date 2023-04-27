870ff7add216fd6792c21e7f6cedf912e02a055b
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-04-27 07:43:31 +0000

    fix unittests

--- lib/feature/tweet/tweet_test.go
+++ lib/feature/tweet/tweet_test.go
@@ -4,4 +4,11 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/syou616
+	"github.com/syou6162/go-active-learning/lib/feature"
+	"github.com/syou6162/go-active-learning/lib/model"
+)
+
+func TestExtractHostFeature(t *testing.T) {
+	e := model.Example{}
+	e.Title = "Hello world"
+	tw
