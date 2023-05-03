964d712f6c7db3d48233fb4b88a221cc1752101f
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-05-03 16:05:02 +0000

    bump copyright

--- lib/fetcher/fetcher_test.go
+++ lib/fetcher/fetcher_test.go
@@ -5,4 +5,10 @@ import (
 	"testing"
 )
 
-fu
+func TestGetArticle(t *testing.T) {
+	a, err := GetArticle("https://www.yasuhisay.info/entry/20090516/1242480413")
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if a.Title =
