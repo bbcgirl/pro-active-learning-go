4b9dd2bef9b0e8d027b0fed98c9550b4e410f2ea
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-05-03 19:59:53 +0000

    README edited

--- lib/fetcher/fetcher_test.go
+++ lib/fetcher/fetcher_test.go
@@ -18,4 +18,13 @@ func TestGetArticle(t *testing.T) {
 		t.Error("Description must not be empty")
 	}
 	if a.OgType != "article" {
-		t.Error("OgType must be
+		t.Error("OgType must be article")
+	}
+	if a.StatusCode != 200 {
+		t.Error("StatusCode must be 200")
+	}
+}
+
+func TestGetArticleARXIV(t *testing.T) {
+	a, err := GetArticle("https://arxiv.org/abs/2012.07805")
+	i
