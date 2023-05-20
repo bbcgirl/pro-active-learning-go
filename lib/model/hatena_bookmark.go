142397095c09643d4152695defe7bba9ed9916d0
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-05-20 14:52:52 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.1-14 (tags/RELEASE_601/final)

--- lib/model/hatena_bookmark.go
+++ lib/model/hatena_bookmark.go
@@ -16,4 +16,12 @@ type HatenaBookmarkTime struct {
 // ref: https://dev.classmethod.jp/go/struct-json/
 func (hbt *HatenaBookmarkTime) UnmarshalJSON(data []byte) error {
 	t, err := time.Parse("\"2006/01/02 15:04\"", string(data))
-	*hbt = 
+	*hbt = HatenaBookmarkTime{&t}
+	return err
+}
+
+func (hbt HatenaBookmarkTime) MarshalJSON() ([]byte, error) {
+	return json.Marshal(hbt.Format("2006/01/02 15:04"))
+}
+
+// ref: https://qi
