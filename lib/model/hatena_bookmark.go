b586666c86d828c4485f9b25bed9a48f197ea009
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-05-22 05:49:18 +0000

    working on cairo

--- lib/model/hatena_bookmark.go
+++ lib/model/hatena_bookmark.go
@@ -24,4 +24,11 @@ func (hbt HatenaBookmarkTime) MarshalJSON() ([]byte, error) {
 	return json.Marshal(hbt.Format("2006/01/02 15:04"))
 }
 
-// ref: https://qi
+// ref: https://qiita.com/roothybrid7/items/52623bedb45ff0c26a8a
+func (hbt *HatenaBookmarkTime) Scan(value interface{}) error {
+	v := value.(time.Time)
+	hbt.Time = &v
+	return nil
+}
+
+func (hbt Hate
