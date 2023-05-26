1e9948ff1af62f65bb024f867227f6bf1fad72b5
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-05-26 16:51:42 +0000

    new

--- lib/model/recommendation.go
+++ lib/model/recommendation.go
@@ -6,4 +6,9 @@ type RecommendationListType int
 
 const (
 	GENERAL RecommendationListType = 0
-	ARTICLE RecommendationList
+	ARTICLE RecommendationListType = 1
+	GITHUB  RecommendationListType = 2
+	SLIDE   RecommendationListType = 3
+	ARXIV   RecommendationListType = 4
+	VIDEO   RecommendationListType = 5
+	EVENT   Recommendati
