8af5873d3a38eedebb4fff2d26f99e3ed88e3ba9
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-05-31 16:35:48 +0000

    Restructured folders

--- lib/related_example/related_example.go
+++ lib/related_example/related_example.go
@@ -11,3 +11,10 @@ import (
 	"github.com/syou6162/go-active-learning/lib/model"
 	"github.com/syou6162/go-active-learning/lib/service"
 	"github.com/urfave/cli"
+)
+
+func parseLine(line string) (int, int, error) {
+	tokens := strings.Split(line, "\t")
+	if len(tokens) == 2 {
+		exampleId, _ := strconv.ParseInt(tokens[0], 10, 0)
+		relatedExampleId, _ := strconv.ParseInt(tokens[
