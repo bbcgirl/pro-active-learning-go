7116b19f0db2561b742bbb579bc1572b73e3d6e8
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-05-31 08:18:42 +0000

    nothing important

--- lib/related_example/related_example.go
+++ lib/related_example/related_example.go
@@ -1,1 +1,13 @@
-pa
+package related_example
+
+import (
+	"bufio"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"os"
+
+	"github.com/syou6162/go-active-learning/lib/model"
+	"github.com/syou6162/go-active-learning/lib/service"
+	"github.com/urfave/cli"
