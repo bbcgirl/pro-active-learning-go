a1bc3eea4e6ab5389149756e75fe38a76c9a3669
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-06-04 20:21:04 +0000

    md: raid0: fix error return from create_stripe_zones.

--- lib/related_example/related_example.go
+++ lib/related_example/related_example.go
@@ -17,4 +17,10 @@ func parseLine(line string) (int, int, error) {
 	tokens := strings.Split(line, "\t")
 	if len(tokens) == 2 {
 		exampleId, _ := strconv.ParseInt(tokens[0], 10, 0)
-		relatedExampleId, _ := strconv.ParseInt(tokens[
+		relatedExampleId, _ := strconv.ParseInt(tokens[1], 10, 0)
+		return int(exampleId), int(relatedExampleId), nil
+	}
+	return 0, 0, fmt.Errorf("Invalid line: %s", line)
+}
+
+func readRelatedExamples(filename string) ([]*model.RelatedExamples, erro
