49f86c5e8a017ed37465cd3f373e3eea88ce11c7
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-06-20 11:21:40 +0000

    Updated ide submodule pointer

--- lib/repository/example.go
+++ lib/repository/example.go
@@ -8,4 +8,11 @@ import (
 	"time"
 
 	"github.com/lib/pq"
-	"github.com/syou6162/go-active-learning
+	"github.com/syou6162/go-active-learning/lib/feature"
+	"github.com/syou6162/go-active-learning/lib/model"
+	"github.com/syou6162/go-active-learning/lib/util/file"
+)
+
+var exampleNotFoundError = model.NotFoundError("example")
+
+// デー
