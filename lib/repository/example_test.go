168ee95d25b058dff35ee13a260a0e9ec0517c26
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-08-03 21:26:35 +0000

    output fix

--- lib/repository/example_test.go
+++ lib/repository/example_test.go
@@ -7,4 +7,9 @@ import (
 	"time"
 
 	"github.com/syou6162/go-active-learning/lib/example"
-	"
+	"github.com/syou6162/go-active-learning/lib/feature"
+	"github.com/syou6162/go-active-learning/lib/model"
+	"github.com/syou6162/go-active-learning/lib/repository"
+)
+
+func TestMai
