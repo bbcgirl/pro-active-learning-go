13bfd3d936919a61275e9890fb58c6527faf6e2d
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-08-18 06:05:49 +0000

    change username

--- lib/repository/example_test.go
+++ lib/repository/example_test.go
@@ -252,4 +252,11 @@ func TestReadLabeledExamples(t *testing.T) {
 	if err != nil {
 		t.Errorf(err.Error())
 	}
-	de
+	defer repo.Close()
+
+	if err = repo.DeleteAllExamples(); err != nil {
+		t.Error(err)
+	}
+
+	err = repo.UpdateOrCreateExample(example.NewExample("http://hoge1.com", model.POSITIVE))
+	if err != 
