87324ad9270925e6ae8bb56eeaace12fe271c61f
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-09-20 20:14:16 +0000

    Version 0.7.5

--- lib/repository/example_test.go
+++ lib/repository/example_test.go
@@ -598,4 +598,9 @@ func TestSearchExamplesByWords(t *testing.T) {
 	examples, err = repo.SearchExamplesByKeywords([]string{"語"}, "ALL", 100)
 	if len(examples) != 2 {
 		t.Errorf("len(examples) == %d, want 2", len(examples))
-	
+	}
+	examples, err = repo.SearchExamplesByKeywords([]string{"日本語", "英語"}, "ALL", 100)
+	if len(examples) != 0 {
+		t.Errorf("len(examples) == %d, want 0", len(examples))
+	}
+	examples, err = repo.Searc
