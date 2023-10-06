04c2d8265d3a38345b6e6fd5cb58b0e86f1ba48b
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-10-06 05:24:23 +0000

    Update fr_CA.po (POEditor.com)

--- lib/repository/mira.go
+++ lib/repository/mira.go
@@ -3,4 +3,12 @@ package repository
 import (
 	"encoding/json"
 
-	"github.co
+	"github.com/syou6162/go-active-learning/lib/classifier"
+)
+
+func (r *repository) InsertMIRAModel(m classifier.MIRAClassifier) error {
+	bytes, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+	q
