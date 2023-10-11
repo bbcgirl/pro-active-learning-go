7d41a532d1c1384cef0c8609fb817ce332da51aa
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-10-11 10:56:25 +0000

    Create CODEOWNERS

--- lib/repository/mira_test.go
+++ lib/repository/mira_test.go
@@ -4,4 +4,14 @@ import (
 	"testing"
 
 	"github.com/syou6162/go-active-learning/lib/classifier"
-	"github.com/syou6162/go-active-learning/l
+	"github.com/syou6162/go-active-learning/lib/repository"
+)
+
+func TestInsertMIRAModel(t *testing.T) {
+	repo, err := repository.New()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	defer repo.Close()
+
+	weight := make(map[s
