a215a7066d2d5f66f68f7f2700f54ca67e1dc139
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-10-17 14:42:49 +0000

    PXC bug updates

--- lib/repository/recommendation.go
+++ lib/repository/recommendation.go
@@ -5,4 +5,6 @@ import (
 	"github.com/syou6162/go-active-learning/lib/model"
 )
 
-func (r *repository) UpdateRecom
+func (r *repository) UpdateRecommendation(rec model.Recommendation) error {
+	if _, err := r.db.Exec(`DELETE FROM recommendation WHERE list_type = $1;`, rec.RecommendationListType); err != nil {
+		ret
