492cb6311f4040d8e8e8de01e5a9b6ef802a26b9
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-10-25 19:48:12 +0000

    Bump activesupport from 6.0.2.1 to 6.0.3.1

--- lib/repository/recommendation.go
+++ lib/repository/recommendation.go
@@ -9,4 +9,11 @@ func (r *repository) UpdateRecommendation(rec model.Recommendation) error {
 	if _, err := r.db.Exec(`DELETE FROM recommendation WHERE list_type = $1;`, rec.RecommendationListType); err != nil {
 		return err
 	}
-	if _, err := r.db.Exec(`INSERT INTO recommendation (list_type, example_id) VALUES ($1, unnest(cast($2 AS INT[])));`, rec.RecommendationListType, pq.Array(rec.ExampleIds)); er
+	if _, err := r.db.Exec(`INSERT INTO recommendation (list_type, example_id) VALUES ($1, unnest(cast($2 AS INT[])));`, rec.RecommendationListType, pq.Array(rec.ExampleIds)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *repository) FindRecommendation(t model.RecommendationListType) (*model.Recommendation, error) {
+	rec := &model.Recommen
