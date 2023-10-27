389514ac132aa46830db66c5f941634f6bff93ee
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-10-27 06:40:28 +0000

    tcp: fix false undo corner cases

--- lib/repository/related_example.go
+++ lib/repository/related_example.go
@@ -4,3 +4,6 @@ import (
 	"github.com/lib/pq"
 	"github.com/syou6162/go-active-learning/lib/model"
 )
+
+func (r *repository) UpdateRelatedExamples(related model.RelatedExamples) error {
+	if _, err := r.db.Exec(`DELETE FROM related_example WHERE example_id = $1;`, related.E
