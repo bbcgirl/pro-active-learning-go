a2c2d8b73bdfe73bbb451b672864636d096eea15
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-10-27 14:41:40 +0000

    fixed method name

--- lib/repository/related_example.go
+++ lib/repository/related_example.go
@@ -9,4 +9,11 @@ func (r *repository) UpdateRelatedExamples(related model.RelatedExamples) error 
 	if _, err := r.db.Exec(`DELETE FROM related_example WHERE example_id = $1;`, related.ExampleId); err != nil {
 		return err
 	}
-	if _, err := r.db.Exec(`INSERT INTO related_example (example_id, related_example_id) VALUES ($1, unnest(cast($2 AS INT[])));`, related.ExampleId, pq.Array(
+	if _, err := r.db.Exec(`INSERT INTO related_example (example_id, related_example_id) VALUES ($1, unnest(cast($2 AS INT[])));`, related.ExampleId, pq.Array(related.RelatedExampleIds)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *repository) FindRelatedExamples(e *model.Example) (*model.RelatedExamples, error) {
+	related := &model.RelatedExampl
