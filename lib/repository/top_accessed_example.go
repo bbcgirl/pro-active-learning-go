8c9411096fb93e521b75b4311afb3e1c29712d72
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-11-27 07:53:50 +0000

    Update sbt to 1.4.8

--- lib/repository/top_accessed_example.go
+++ lib/repository/top_accessed_example.go
@@ -1,4 +1,11 @@
 package repository
 
 import (
-	"github.com
+	"github.com/lib/pq"
+)
+
+func (r *repository) UpdateTopAccessedExampleIds(exampleIds []int) error {
+	if _, err := r.db.Exec(`DELETE FROM top_accessed_example;`); err != nil {
+		return err
+	}
+	if _, err :
