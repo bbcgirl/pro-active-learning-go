5e012bf6fbc2299ac38e94e9ddd7669c9bd46cf0
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-11-27 15:28:43 +0000

    Fix typos in docs

--- lib/repository/top_accessed_example.go
+++ lib/repository/top_accessed_example.go
@@ -8,4 +8,10 @@ func (r *repository) UpdateTopAccessedExampleIds(exampleIds []int) error {
 	if _, err := r.db.Exec(`DELETE FROM top_accessed_example;`); err != nil {
 		return err
 	}
-	if _, err :
+	if _, err := r.db.Exec(`INSERT INTO top_accessed_example (example_id) VALUES (unnest(cast($1 AS INT[])));`, pq.Array(exampleIds)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *rep
