d1791518a15eae67ef60b345e103c13fd58830a8
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-11-29 08:20:20 +0000

    tweakage

--- lib/repository/top_accessed_example_test.go
+++ lib/repository/top_accessed_example_test.go
@@ -4,4 +4,10 @@ import (
 	"testing"
 
 	"github.com/syou6162/go-active-learning/lib/example"
-	"github.com/syou6162/go
+	"github.com/syou6162/go-active-learning/lib/model"
+	"github.com/syou6162/go-active-learning/lib/repository"
+)
+
+func TestUpdateTopAccessedExampleIds(t *testing.T) {
+	repo, err := repository.New()
+	if err != nil {
