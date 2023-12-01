47fcc1b964098296acd1a1870ccf4686edd71e55
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-12-01 06:53:40 +0000

    binary

--- lib/repository/tweet.go
+++ lib/repository/tweet.go
@@ -1,3 +1,11 @@
 package repository
 
-impor
+import (
+	"time"
+
+	"github.com/lib/pq"
+	"github.com/syou6162/go-active-learning/lib/model"
+)
+
+func (r *repository) UpdateOrCreateReferringTweets(e *model.Example) error {
+	if e.Referring
