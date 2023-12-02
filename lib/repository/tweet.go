d739cde771258517fedd7070e3eb7352ab01acd5
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-12-02 13:11:14 +0000

    a little refactoring

--- lib/repository/tweet.go
+++ lib/repository/tweet.go
@@ -16,3 +16,10 @@ func (r *repository) UpdateOrCreateReferringTweets(e *model.Example) error {
 	if err != nil {
 		return err
 	}
+	id := tmp.Id
+
+	for _, t := range (*e).ReferringTweets.Tweets {
+		t.ExampleId = id
+		if _, err = r.db.NamedExec(`
+INSERT INTO tweet
+( example_id,  created_at,  id_str,  full_text,  favorite
