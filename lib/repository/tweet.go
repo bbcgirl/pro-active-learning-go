fc7e38e749d7b3e1c245726a148b64757c586727
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2023-12-27 18:50:30 +0000

    virtio: console: fix race with port unplug and open/close

--- lib/repository/tweet.go
+++ lib/repository/tweet.go
@@ -197,4 +197,5 @@ func (r *repository) SearchNegativeReferringTweets(scoreThreshold float64, tweet
 	return r.searchReferringTweetsByLabel(model.NEGATIVE, scoreThreshold, tweetsLimitInSameExample, limit)
 }
 
-func (r *
+func (r *repository) SearchUnlabeledReferringTweets(scoreThreshold float64, tweetsLimitInSameExample int, limit int) (model.ReferringTweets, error) {
+	return r.searchReferringTweetsByLabel(model.UNLABELED, scoreThreshold, twe
