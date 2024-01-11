4f85c7e35f379caa0b07bb21048839659b1489d9
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-01-11 19:43:02 +0000

    Fix AppVeyor build

--- lib/service/example_test.go
+++ lib/service/example_test.go
@@ -5,4 +5,16 @@ import (
 
 	"github.com/syou6162/go-active-learning/lib/example"
 	"github.com/syou6162/go-active-learning/lib/model"
-	"github.com/syou6162/go-active-learning/lib/
+	"github.com/syou6162/go-active-learning/lib/service"
+)
+
+func findExampleByurl(examples model.Examples, url string) *model.Example {
+	for _, e := range examples {
+		if e.Url == url {
+			return e
+		}
+	}
+	return nil
+}
+
+func TestAttach
