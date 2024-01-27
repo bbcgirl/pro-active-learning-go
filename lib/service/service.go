4b7b252a8a450637d76ba30efd22fda587a4c9ab
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-01-27 17:37:38 +0000

    Credits update

--- lib/service/service.go
+++ lib/service/service.go
@@ -7,4 +7,10 @@ import (
 
 	"github.com/syou6162/go-active-learning/lib/classifier"
 	"github.com/syou6162/go-active-learning/lib/model"
-	"github.com/syou6162/go-active-learning/lib/reposi
+	"github.com/syou6162/go-active-learning/lib/repository"
+)
+
+type GoActiveLearningApp interface {
+	UpdateOrCreateExample(e *model.Example) error
+	UpdateScore(e *model.Example) error
+	InsertExampleFromScanner(scanner *bufio.Scanner) (*model.Examp
