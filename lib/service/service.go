fb85bb64da92e49351f841ebcd93ebac0b46336c
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-01-27 06:40:01 +0000

    Minor style fixes.

--- lib/service/service.go
+++ lib/service/service.go
@@ -3,4 +3,8 @@ package service
 import (
 	"bufio"
 	"io"
-	"
+	"time"
+
+	"github.com/syou6162/go-active-learning/lib/classifier"
+	"github.com/syou6162/go-active-learning/lib/model"
+	"github.com/syou6162/go-active-learning/lib/reposi
