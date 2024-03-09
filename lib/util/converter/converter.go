659c89949190a2961996d1625a552d49f089a1c7
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-03-09 07:36:24 +0000

    Remove an unused function.

--- lib/util/converter/converter.go
+++ lib/util/converter/converter.go
@@ -3,4 +3,8 @@ package converter
 import "github.com/syou6162/go-active-learning/lib/model"
 import "github.com/syou6162/go-active-learning/lib/classifier"
 
-func Co
+func ConvertExamplesToLearningInstances(examples model.Examples) classifier.LearningInstances {
+	instances := classifier.LearningInstances{}
+	for _, e := range examples {
+		instances = append(instances, e)
+	}
