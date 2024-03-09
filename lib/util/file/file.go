6678a14ac62ed873b933084e3a6e29e1854c5459
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-03-09 13:50:11 +0000

    add missing class

--- lib/util/file/file.go
+++ lib/util/file/file.go
@@ -9,4 +9,11 @@ import (
 	"strings"
 
 	"github.com/syou6162/go-active-learning/lib/example"
-	"github.com/sy
+	"github.com/syou6162/go-active-learning/lib/model"
+)
+
+func ParseLine(line string) (*model.Example, error) {
+	tokens := strings.Split(line, "\t")
+	var url string
+	if len(tokens) == 1 {
+		url = tok
