1bdfdefb638a5d6fc70cd3a1b7d1d42971244dd7
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-03-26 11:32:40 +0000

    updated example code

--- lib/util/util_test.go
+++ lib/util/util_test.go
@@ -2,3 +2,10 @@ package util
 
 import (
 	"testing"
+
+	"github.com/syou6162/go-active-learning/lib/example"
+	"github.com/syou6162/go-active-learning/lib/model"
+)
+
+func TestFilterLabeledExamples(t *testing.T) {
+	e1 := example.NewExample("https://b.h
