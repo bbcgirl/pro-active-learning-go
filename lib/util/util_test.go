b9575e5f717eb3a78fe3b98672f753a15f5bc79e
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-03-26 11:33:58 +0000

    - debug

--- lib/util/util_test.go
+++ lib/util/util_test.go
@@ -8,4 +8,8 @@ import (
 )
 
 func TestFilterLabeledExamples(t *testing.T) {
-	e1 := example.NewExample("https://b.h
+	e1 := example.NewExample("https://b.hatena.ne.jp", model.POSITIVE)
+	e2 := example.NewExample("https://www.yasuhisay.info", model.NEGATIVE)
+	e3 := example.NewExample("http://google.com", model.UNLABELED)
+
+	examples := FilterLabeledExamples(m
