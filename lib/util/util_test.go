49976a96719c8b53d784e54c2d5cb985d27c7025
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-03-27 20:13:24 +0000

    Remove print

--- lib/util/util_test.go
+++ lib/util/util_test.go
@@ -19,4 +19,7 @@ func TestFilterLabeledExamples(t *testing.T) {
 }
 
 func TestFilterUnlabeledExamples(t *testing.T) {
-	e1 := example.NewExample("h
+	e1 := example.NewExample("https://b.hatena.ne.jp", model.POSITIVE)
+	e2 := example.NewExample("https://www.yasuhisay.info", model.NEGATIVE)
+	e3 := example.NewExample("http://google.com", model.UNLABELED)
+	e3.Title = 
