bd3dd055567160c4d00f6fb5a620207fc827ba78
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-04-07 22:06:15 +0000

    Small update to README

--- migrations/10.sql
+++ migrations/10.sql
@@ -1,2 +1,5 @@
 -- +migrate Up
-CREATE TAB
+CREATE TABLE IF NOT EXISTS recommendation (
+  "list_type" INT NOT NULL,
+  "example_id" SERIAL NOT NULL,
+   CONSTRAINT recommendation_example_id_fkey FOREIGN KEY ("example_id") REFERENCES example("id") ON UPDATE N
