38f6caefe7c2509084ac96806634e7bc66982c69
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-04-10 10:44:22 +0000

    minor renaming

--- migrations/10.sql
+++ migrations/10.sql
@@ -2,4 +2,12 @@
 CREATE TABLE IF NOT EXISTS recommendation (
   "list_type" INT NOT NULL,
   "example_id" SERIAL NOT NULL,
-   CONSTRAINT recommendation_example_id_fkey FOREIGN KEY ("example_id") REFERENCES example("id") ON UPDATE N
+   CONSTRAINT recommendation_example_id_fkey FOREIGN KEY ("example_id") REFERENCES example("id") ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS "list_type_idx_recommendation" ON recommendation ("list_type");
+
+-- +migrate Down
+DROP INDEX "list_type_idx_recommendation";
+
+DROP TABLE recommendation;
