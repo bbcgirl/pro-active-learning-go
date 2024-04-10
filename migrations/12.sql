0b3cc97750c76d86e2b936c36e174becb6c96c84
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-04-10 20:56:44 +0000

    Add materials

--- migrations/12.sql
+++ migrations/12.sql
@@ -1,1 +1,5 @@
--- +migrate
+-- +migrate Up
+ALTER TABLE "tweet" ADD COLUMN "label" INT NOT NULL DEFAULT 0;
+
+-- +migrate Down
+ALTER TABLE "tweet" DROP COLUMN "label";
---
+++ migrations/13.sql
@@ -0,0 +1,2 @@
+-- +migrate Up
+ALTER TABLE "model" ADD COLUMN "model_type" 
