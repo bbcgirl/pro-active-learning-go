09ef79d8dc42104671af0fd57d238c0514637133
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-04-14 14:10:37 +0000

    Automatic Deploy

--- migrations/15.sql
+++ migrations/15.sql
@@ -1,3 +1,5 @@
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS related_example (
-  "example_id" SER
+  "example_id" SERIAL NOT NULL,
+  "related_example_id" SERIAL NOT NULL,
+  CONSTRAINT related_example_example_id_fkey FOREIGN KEY ("example_id") REFERENCES example("id") ON UPDATE NO ACTION ON DELE
