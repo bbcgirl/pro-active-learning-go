40529714866b916a64f5701191b19ecfe055ff60
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-04-14 16:04:24 +0000

    trival

--- migrations/15.sql
+++ migrations/15.sql
@@ -2,4 +2,6 @@
 CREATE TABLE IF NOT EXISTS related_example (
   "example_id" SERIAL NOT NULL,
   "related_example_id" SERIAL NOT NULL,
-  CONSTRAINT related_example_example_id_fkey FOREIGN KEY ("example_id") REFERENCES example("id") ON UPDATE NO ACTION ON DELE
+  CONSTRAINT related_example_example_id_fkey FOREIGN KEY ("example_id") REFERENCES example("id") ON UPDATE NO ACTION ON DELETE CASCADE,
+  CONSTRAINT related_example_related_example_id_fkey FOREIGN KEY ("related_example_id") REFERENCES example("id") ON UPDATE NO ACTION ON DELETE CASCADE,
+  CHECK(example_id != r
