234cf41f3d5e7c0e1525c029749702b0daa1473d
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-04-25 18:43:44 +0000

    updating link

--- migrations/5.sql
+++ migrations/5.sql
@@ -6,4 +6,9 @@ CREATE TABLE IF NOT EXISTS hatena_bookmark (
   "screenshot" TEXT NOT NULL,
   "entry_url" TEXT NOT NULL,
   "count" INT NOT NULL,
-  
+  "url" TEXT NOT NULL,
+  "eid" TEXT NOT NULL,
+  CONSTRAINT hatena_bookmark_example_id_fkey FOREIGN KEY ("example_id") REFERENCES example("id") ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE UNIQU
