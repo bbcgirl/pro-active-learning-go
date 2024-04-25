475e2f712933c37da163d7eb35c39ffd66f69290
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-04-25 07:07:20 +0000

    linting.

--- migrations/2.sql
+++ migrations/2.sql
@@ -2,4 +2,7 @@
 CREATE TABLE IF NOT EXISTS feature (
   "example_id" SERIAL NOT NULL,
   "feature" TEXT NOT NULL,
-  CONSTRAINT featu
+  CONSTRAINT feature_example_id_fkey FOREIGN KEY ("example_id") REFERENCES example("id") ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS "example_id_idx_example" ON feature ("exam
