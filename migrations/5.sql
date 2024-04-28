92047289ed7d0428abfae7dfde8d40c44e787758
bbcgirl <157942833+bbcgirl@users.noreply.github.com>
2024-04-28 06:52:04 +0000

    test: new neuron

--- migrations/5.sql
+++ migrations/5.sql
@@ -11,4 +11,7 @@ CREATE TABLE IF NOT EXISTS hatena_bookmark (
   CONSTRAINT hatena_bookmark_example_id_fkey FOREIGN KEY ("example_id") REFERENCES example("id") ON UPDATE NO ACTION ON DELETE CASCADE
 );
 
-CREATE UNIQU
+CREATE UNIQUE INDEX IF NOT EXISTS "example_id_idx_hatena_bookmark" ON hatena_bookmark ("example_id");
+CREATE UNIQUE INDEX IF NOT EXISTS "url_idx_hatena_bookmark" ON hatena_bookmark ("url");
+
+CREATE TABLE IF NOT E
